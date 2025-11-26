3e291e0024b9f09d89d21d3ca869fc23381f200a
Jaedon Cox <jtc8866@mavs.uta.edu>
2025-11-26 10:46:50 -0600

    fix

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,7 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
     
+    public GameObject MainMenuUI;
+    public GameObject DeckBuilderUI;
+    public GameObject ClanUI;
+
     public void LoadBattleground(){
         SceneManager.LoadScene("Battleground");
+    }
+
+    public void OpenDeckBuilder(){
+        MainMenuUI.SetActive(false);
+        DeckBuilderUI.SetActive(true);
+    }
+    public void DeckBuilderConfirm(){
+        DeckBuilderUI.SetActive(false);
+        MainMenuUI.SetActive(true);
+    }
+
+    public void OpenClan(){
+        MainMenuUI.SetActive(false);
+        ClanUI.SetActive(true);
+    }
+    public void ClanReturn(){
+        ClanUI.SetActive(false);
+        MainMenuUI.SetActive(true);
     }
 }
