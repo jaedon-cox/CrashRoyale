2a620a58d1c24b9990d0c2a1caf61d27dd7cf17e
Dat Nguyen <83878004+DaQ-Ng@users.noreply.github.com>
2025-12-02 00:21:05 -0600

    Save Battle Deck Back End

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -11,6 +11,12 @@ public class Slot : MonoBehaviour, IDropHandler
             GameObject dropped = eventData.pointerDrag;
             DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
             draggableItem.parentAfterDrag = transform;
+
+            // Save this card to PlayerPrefs
+            PlayerPrefs.SetInt("DeckSlot_" + slotIndex, draggableItem.cardID);
+            PlayerPrefs.Save();
+
+            Debug.Log("Saved card " + draggableItem.cardID + " to slot " + slotIndex);
         }
     }
 }
